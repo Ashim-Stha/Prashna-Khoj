c40ef111bdce07a6333c6d8ea5a8935af9931cc3
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-01-29 09:01:56 +0545

    minor fix

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
